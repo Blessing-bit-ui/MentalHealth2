b5fc9fd970d9cf7a5c29e252cdbec33a6c66fe6e
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-04-27 09:52:04 -0700

    added styling to the Mystory and HOME

--- my-mental/src/Home.jsx
+++ my-mental/src/Home.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 function Home(){
     return(
-        <div className="flex flex-col items-center justify-center min-h-[80vh] text-center px-4">
+        <div className="flex flex-col items-center justify-center min-h-[80vh] text-center px-4 shadow shadow-3xl">
             <h1 className="text-blue-600 text-center font-bold text-6xl"> Welcome to Mindful Space</h1>
             <p className=" text-gray-700 text-lg max-w-l text-center mt-8"> A safe place to learn about mental, find resources and take steps toward a healthier mind and heart.</p>
             <Link to="/about"className="text-blue-700 hover:underline">Learn More</Link>
--- my-mental/src/MyStory.jsx
+++ my-mental/src/MyStory.jsx
@@ -1,18 +1,18 @@
 function MyStory(){
     return(
         <div className="flex flex-col items-center justify-center min-h-[80vh] text-center px-4 bg-purple-600 shadow shadow-2xl mt-8">
-        <h1 className="text-4xl font-bold text-teal-400 mb-6">My Story</h1>
+        <h1 className="text-6xl font-bold text-orange-300 mb-6 italic">My Story</h1>
         <section className="mb-6">
-        <h2 className="text-2xl font-semibold text-teal-500 mb-2"> Why I created This Platform</h2>
-        <p> I've always been passionate about mental health of my own struggles and experiences. I know how important it is to find support, resources, and understand during tough times. That's why I created this platform - to share what I've learned and to provide a space for other to heal and grow.</p>
+        <h2 className="text-3xl font-semibold text-orange-300 mb-2 italic"> Why I created This Platform</h2>
+        <p className="text-2xl text-white"> I've always been passionate about mental health of my own struggles and experiences. I know how important it is to find support, resources, and understand during tough times. That's why I created this platform - to share what I've learned and to provide a space for other to heal and grow.</p>
         </section>
         <section className="mb-6">
-            <h2 className="text-2xl font-semibold text-teal-500 mb-2"> My Journey</h2>
-            <p className=" text-gray-700"> My own journey with mental health was not easy, and I've faced moments of doubt and anxiety. But over time, I found that opening up, seeking help, and understanding myself better help others go others go through a similar process and show that mental health is something we can all work on, together.</p>
+            <h2 className="text-2xl font-semibold text-orange-300 italic text-3xl mb-2"> My Journey</h2>
+            <p className=" text-2xl text-white"> My own journey with mental health was not easy, and I've faced moments of doubt and anxiety. But over time, I found that opening up, seeking help, and understanding myself better help others go others go through a similar process and show that mental health is something we can all work on, together.</p>
         </section>
         <section className="mb-6">
-        <h2 className="text-2xl font-semibold text-teal-500 mb-2">Let's Connect</h2>
-        <p className=" text-gray-700"> If your have questions, want to talk, or need someone to listen, feel free to reach out. You're not alone on this journey.</p>
+        <h2 className="text-2xl font-semibold text-orange-300 mb-2">Let's Connect</h2>
+        <p className=" text-white text-2xl"> If your have questions, want to talk, or need someone to listen, feel free to reach out. You're not alone on this journey.</p>
         </section>
         </div>
     )
