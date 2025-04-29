49eb813a5ef0adb8d0738da325fe74135a6705f3
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-04-29 14:05:20 -0700

    added styling to support

--- my-mental/src/Support.jsx
+++ my-mental/src/Support.jsx
@@ -1,18 +1,30 @@
 function Support(){
-    return(
-        <div>
-        <h1> Support</h1>
-        <section>
-            <h2> Contact Us</h2>
-            <p> If you need assistance, feel free to reach out to us via email at yembeblessing27@gmail.com</p>
-        </section>
-        <section>
-            <h2>Frequently Asked Question</h2>
-           <ul>
-            <l1><strong>How do i track my mental health? </strong> Check the Resources section for mental health profession.</l1>
-            </ul> 
-        </section>
+    return (
+      <div className="flex flex-col items-center justify-center min-h-[80vh] text-center ">
+        <h1 className="text-4xl font-bold text-orange-600 mb-6 text-center">
+          Support
+        </h1>
+        <div className="mb-6 w-min md:w-full">
+          <h2 className="text-2xl  text-center font-semibold text-orange-500 mb-2">
+            Contact Us
+          </h2>
+          <p className="text-gray-700 leading-relaxed">
+            If you need assistance, feel free to reach out to us via email at
+<span className="text-orange-300 hover:underline"> yembeblessing27@gmail.com</span>
+          </p>
         </div>
-    )
+        <div>
+          <h2 className="text-2xl font-semibold text-orange-500 mb-2">
+            Frequently Asked Question
+          </h2>
+          <ul className="list-disc pl-6 text-gray-700">
+            <l1>
+              <strong>Where can I find Help?</strong> Check the Resources
+              section for mental health profession.
+            </l1>
+          </ul>
+        </div>
+      </div>
+    );
 }
 export default Support;
